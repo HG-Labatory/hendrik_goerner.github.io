e3e4b00bce9d2c3e80f8362f7349c2536fea20de
HG <hendrikgoerner1337@gmail.com>
2025-12-31 10:36:32 +0100

    update astro.config.mjs and src/config.ts with personal details

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ryanfitzgerald.github.io",
-  base: "devportfolio",
+  site: "https://hg-labatory.github.io/hendrik_goerner.github.io/",
+  base: "/hendrik_goerner.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
