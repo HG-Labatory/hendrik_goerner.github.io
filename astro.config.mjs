24ead5d9f0a3625769487f7821a6db9d56ea09df
HG <hendrikgoerner1337@gmail.com>
2025-12-31 10:49:04 +0100

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://hg-labatory.github.io/hendrik_goerner.github.io/",
-  base: "/hendrik_goerner.github.io",
+  base: "/hendrik_goerner.github.io/gh_pages",
   vite: {
     plugins: [tailwindcss()],
   },
