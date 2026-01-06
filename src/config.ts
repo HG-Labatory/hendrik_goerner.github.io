01069a90f452342aacd090cf2c5773a8b6c51c4c
HG <hendrikgoerner1337@gmail.com>
2026-01-06 17:47:05 +0100

    update config

--- src/config.ts
+++ src/config.ts
@@ -35,7 +35,7 @@ export const siteConfig = {
       name: "Trafficnews Dashboard",
       description:
         "Das System sammelt Verkehrsmeldungen aus verschiedenen Quellen per Scraper, normalisiert sie in ein einheitliches Format und speichert sie in einer PostgreSQL-Datenbank und in einem zentralen Traffic-Dashboard gebündelt dargestellt.",
-      link: "https://github.com/HG-Labatory/TrafficDashboard?tab=readme-ov-file#trafficdashboard",
+      link: "https://github.com/HG-Labatory/TrafficDashboard",
       skills: ["Python","PostgreSQL", "GitHub Pages", "Webentwicklung", "Webscraping"],
     },
     {
